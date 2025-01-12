d0f062075b320a7d64dcd3cf86d269c660214a09
sdfsdfsdwe12 <ww67150.14@gmail.com>
2025-01-12 20:29:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     numberOfRegisterInOneTime: 6, // no of registration want to make in one command.
     domains: ["lamoshitt.com", "crazzybabe.com"], // names of domain for the emails. 
     oneTimeMailGen: 10 * 100000,
-    telegramBotToken: "7387279346:AAGdIyOX5qb61Lj_4upbbU8smUSdouMCR94", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
-    adminNamesForTelegramBot: ["aarav_0008","hahaha7726"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
-    authorisedUserForTelegramBot: ["aarav_0008","hahaha7726"] // username or id of the user who can use the bot.
+    telegramBotToken: "7877031785:AAESstMeLGjopaNMy-nuhKIOUzUtYYGLObw", // bot token of telegram bot. Create a bot using https://t.me/BotFather then generate the token of the bot.
+    adminNamesForTelegramBot: ["Mrrandom"], // name of the admin/developer who's name will be displayed when any unknown user use the command of the bot in telegram.
+    authorisedUserForTelegramBot: ["5142603617","Satyam2322"] // username or id of the user who can use the bot.
 };
